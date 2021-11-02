b31e2dc80d21ff9c71cf95af83f8ab04af3eb10b
kenan06-art <78687637+kenan06-art@users.noreply.github.com>
2021-11-02 19:58:10 +0300

    Add files via upload

---
+++ SUtku-ModerasyonAltYapı/komutlar/yardim.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+const embed = new Discord.MessageEmbed()
+.setColor('RANDOM')
+.setTitle('🔴 FREK Bot Tüm Komutlar 🔴')
+.setTimestamp()
+.addField('🟢 Sunucuda AFK Kalabilmek için', '!afk [Neden AFK kaldığınız dilerseniz]')
+.addField('🟢 Herhangi Bir Üyeyi Sunucudan Banlamak için', '!ban [kullanıcı]')
+.addField('🟢 Sunucuda Bir Üyenin Banını Açmak İçin', '!unban [kullanıcı]')
+.addField('🟢 Herhangi Bir Üyeyi Seste Susturmak için', '!vmute [kullanıcı] [süre] [sebep]')
+.addField('🟢 Herhangi Bir Üyeyi Chatte Susturmak için', '!tmute [kullanıcı] [süre] [sebep]')
+.addField('🟢 Herhangi Bir Üyeye Jail Atmak için', '!jail [kullanıcı] [süre] [sebep]')
+.addField('🟢 Sunucuda Küfür Filtresini Aktif Etmek için', '!küfür aç-kapat')
+.addField('🟢 Herhangi Bir Sohbet Kanalını Temizlemek için', '!temizle [adet]   !sil [adet]')
+.addField('🟢 Sunucudaki Üyeleri Öğrenmek için', '!say')
+.setFooter('© 2021 Olimpos Moderation', client.user.avatarURL)
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: ['yardım', 'yardim'], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım'
+};
